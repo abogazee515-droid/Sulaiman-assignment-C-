556864194b46f636ade4431b6ce6f442a21aed8c
abogazee515-droid <abogazee515@gmail.com>
2026-03-10 11:30:48 +0700

    Update function_no_parameter.cpp

--- function_no_parameter.cpp
+++ function_no_parameter.cpp
@@ -3,33 +3,34 @@
 #include <iostream>
 using namespace std;
 
-// Variables for rectangle dimensions
+
 int length, width;
-// Function to get input from user
+
 void input()
 { 
-    // Ask user to enter rectangle length
+    
     cout << "Enter length: ";
     cin >> length;
-// Ask user to enter rectangle width
+
     cout << "Enter width: ";
     cin >> width;
 }
-// Function to calculate rectangle area
+
 int rectangleArea()
 {
     return length * width;
 }
-// Function to display result
+
 void output()
 {
     cout << "The area is: " << rectangleArea();
 }
-// Main function
+
 int main()
 {
     input();
     output();
 
 }
+
 
