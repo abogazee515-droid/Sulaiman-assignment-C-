63a6cb79290b791d791ce22457a0ceb9b3377192
abogazee515-droid <abogazee515@gmail.com>
2026-03-10 11:55:57 +0700

    Add function with parameter program

--- function_no_parameter.cpp
+++ function_no_parameter.cpp
@@ -1,47 +1,29 @@
-// Assignment: Function Without Parameter Example
-// C++ rectangle area program
 #include <iostream>
 using namespace std;
 
-// Variables for rectangle dimensions
 int length, width;
-//function to enter the rectangle dimensions
-void input()
 
-{ 
-    // Ask user to enter rectangle length
-    cout << " Enter length: ";
+void input()
+{
+    cout << "Enter length: ";
     cin >> length;
-// Ask user to enter rectangle width
-    cout << " Enter width: ";
+
+    cout << "Enter width: ";
     cin >> width;
 }
-// Function to calculate rectangle area
+
 int rectangleArea()
 {
     return length * width;
 }
-// Function to display result
+
 void output()
 {
     cout << "The area is: " << rectangleArea();
 }
-// Main function
+
 int main()
 {
     input();
     output();
-
 }
-
-
-
-
-
-
-
-
-
-
-
-
