eb82d732a7771dcaea665931ae3b026ae887345e
abogazee515-droid <abogazee515@gmail.com>
2026-03-10 11:14:34 +0700

    Final update for assignment

--- function_no_parameter.cpp
+++ function_no_parameter.cpp
@@ -1,29 +1,35 @@
+// Assignment: Function Without Parameter Example
+// C++ rectangle area program
 #include <iostream>
 using namespace std;
 
+// Variables for rectangle dimensions
 int length, width;
-
+// Function to get input from user
 void input()
-{
+{ 
+    // Ask user to enter rectangle length
     cout << "Enter length: ";
     cin >> length;
-
+// Ask user to enter rectangle width
     cout << "Enter width: ";
     cin >> width;
 }
-
+// Function to calculate rectangle area
 int rectangleArea()
 {
     return length * width;
 }
-
+// Function to display result
 void output()
 {
     cout << "The area is: " << rectangleArea();
 }
-
+// Main function
 int main()
 {
     input();
     output();
+
 }
+
