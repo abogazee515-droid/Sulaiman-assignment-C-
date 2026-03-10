b240c71c9bf7b6b8e8e03b6d1b56208f4a436e01
abogazee515-droid <abogazee515@gmail.com>
2026-03-10 12:01:08 +0700

    Add function with parameter program

---
+++ function_with_parameter.cpp.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+using namespace std;
+
+int length, width;
+
+void input()
+{
+    cout << "Enter length: ";
+    cin >> length;
+
+    cout << "Enter width: ";
+    cin >> width;
+}
+
+int rectangleArea(int a, int b)
+{
+    return a * b;
+}
+
+int rectanglePerimeter(int a, int b)
+{
+    return 2 * (a + b);
+}
+
+void output()
+{
+    cout << "Area: " << rectangleArea(length, width) << endl;
+}
+
+int main()
+{
+    input();
+    output();
+    cout << "Perimeter: " << rectanglePerimeter(length, width);
+}
